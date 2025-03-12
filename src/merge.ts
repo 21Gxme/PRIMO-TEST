f4e1e92f0e653b5c87e995dd34de015015f47130
Tantikon Phasanphaengsi <tantikon.p@ku.th>
2025-03-12 19:06:44 +0700

    edit merge test

--- src/merge.ts
+++ src/merge.ts
@@ -1,23 +1,41 @@
-export function merge(collection_1: number[], collection_2: number[]): number[] {
+export function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[] {
     let merged: number[] = [];
-    let index1 = 0, index2 = 0;
-  
-    while (index1 < collection_1.length && index2 < collection_2.length) {
-      if (collection_1[index1] < collection_2[index2]) {
-        merged.push(collection_1[index1++]);
+    let i = 0, j = 0;
+    
+    while (i < collection_1.length && j < collection_2.length) {
+      if (collection_1[i] < collection_2[j]) {
+        merged.push(collection_1[i++]);
       } else {
-        merged.push(collection_2[index2++]);
+        merged.push(collection_2[j++]);
       }
     }
-  
-    while (index1 < collection_1.length) {
-      merged.push(collection_1[index1++]);
+    
+    while (i < collection_1.length) {
+      merged.push(collection_1[i++]);
     }
-  
-    while (index2 < collection_2.length) {
-      merged.push(collection_2[index2++]);
+    
+    while (j < collection_2.length) {
+      merged.push(collection_2[j++]);
     }
-  
-    return merged;
+    
+    let result: number[] = [];
+    let k = 0, m = 0;
+    
+    while (k < merged.length && m < collection_3.length) {
+      if (merged[k] < collection_3[m]) {
+        result.push(merged[k++]);
+      } else {
+        result.push(collection_3[m++]);
+      }
+    }
+    
+    while (k < merged.length) {
+      result.push(merged[k++]);
+    }
+    
+    while (m < collection_3.length) {
+      result.push(collection_3[m++]);
+    }
+    
+    return result;
   }
-  
